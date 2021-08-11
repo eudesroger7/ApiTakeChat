f7ed9d91abb52f62a6fd8b7f4d0c6c2737eb336d
Eudes Roger <eudesroger7@gmail.com>
2021-08-11 17:28:03 -0300

    users

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,10 @@
 const {Router} = require('express');
 const RepositoriesController = require('./controllers/repositories');
+const usersController = require('./controllers/users');
 
 const routes = Router();
 
 routes.get('/repositories/:username', RepositoriesController.index);
+routes.get('/users/:username', usersController.index);
 
 module.exports = routes;
